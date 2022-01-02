ee6bb825f81cb6d1155dcff44f7ba33111694afc
mahmoud ayman <mahmoudayman409@gmail.com>
2022-01-02 20:54:01 +0200

    footer update

---
+++ footer.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer class="footerall row">
+        <div>
+            <section class="footer-part1">
+                <div class="footer-part1-1">
+                    <h4>more about Resturant</h4>
+                    <p>in our smart Resturantyou can you can enjoy your time
+                        <br> and save your time book a table Easily every thing
+                        <br> is measured for your happiness
+                    </p>
+                    <p class="ceo">-7ox fox CEO</p>
+                </div>
+                <div class="footer-part1-2">
+                    <h4>keep connected</h4>
+                    <div>
+                        <div class="footer-icon">
+                            <img src="assets/facebook.png" alt="facebook">
+                            <p>like us on facebook</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/twitter.png" alt="twitter">
+                            <p>follow us on twitter</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/instagram.png" alt="instagram">
+                            <p>follow us on instagram</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/pinterest.png" alt="pinterest">
+                            <p>follow us on pinterest</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/whatsapp.png" alt="whatsapp">
+                            <p>message us on whatsapp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-part1-3">
+                    <h4>contact information</h4>
+                    <div>
+                        <div class="footer-icon">
+                            <img src="assets/home (1).png" alt="home">
+                            <p>4th Floor, Plot No.22,<br>
+                                145 Murphy Canyon Rd.<br>
+                                San Diego CA 2028</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/email.png" alt="email">
+                            <p>restro.smarto@gmsil.com</p>
+                        </div>
+                        <div class="footer-icon">
+                            <img src="assets/smartphone.png" alt="smartphone">
+                            <p>(060) 444 434 444</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="footer-part2">
+                <div class="footer-part2-1">
+                    <p>Copyright Â© 2021 Simone. All Rights Reserved.</p>
+                    <div class="line"></div>
+                    <h4>Restro</h4>
+                </div>
+                <div class="footer-part2-2">
+                    <p>company information</p>
+                    <div class="line"></div>
+                    <p>Terms & Policy</p>
+                    <div class="line"></div>
+                    <p>our team</p>
+                </div>
+            </section>
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
