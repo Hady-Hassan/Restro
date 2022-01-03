749ec6b11e6a037dd6681fa9ab540bf0851b692d
Hady-Hassan <90941057+Hady-Hassan@users.noreply.github.com>
2022-01-03 17:01:25 +0200

    home

--- navbar.php
+++ navbar.php
@@ -12,10 +12,6 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">  
   <title><?php echo $page_title; ?></title>
-<<<<<<< HEAD
-  <script src="js/jquery-3.6.0.min.js"></script>
-=======
->>>>>>> bf794804aaf0390b5a4d39711bd79ce505663249
  <style>
 
   </style>
@@ -23,30 +19,18 @@
 
 <body class="">
   <!-- Start Navbar -->
-<<<<<<< HEAD
-  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FF9900;">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="images\logo.png" width="70" height="70" alt="" style="margin-right: 50px;">
-      </a>
-=======
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FF9900; margin: bottom 5px;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="images\logo.png" width="70" height="70" alt="" style="margin-right: 50px;">
         </a>
->>>>>>> bf794804aaf0390b5a4d39711bd79ce505663249
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-<<<<<<< HEAD
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-=======
       <div class="collapse navbar-collapse  collapse show" id="navbarSupportedContent">
 
->>>>>>> bf794804aaf0390b5a4d39711bd79ce505663249
         <ul class="navbar-nav" style = "margin: auto">
           <li class="nav-item active">
             <a class="nav-link" href="index.php" style="margin-left: 60px;">Food <span class="sr-only">(current)</span></a>
@@ -63,13 +47,9 @@
           </li>
 
         </ul>
-<<<<<<< HEAD
-        <div class="dropdown ml-auto" style="margin-right: 17px;">
-=======
 
 
         <div class="dropdown" style="">
->>>>>>> bf794804aaf0390b5a4d39711bd79ce505663249
         <button class="btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown"
           aria-haspopup="true" style="background-color: #D58206;" aria-expanded="false">
           <i class="fa fa-user-circle" aria-hidden="true"></i> My Account
@@ -88,16 +68,9 @@
         
       </div>
     </div>
-<<<<<<< HEAD
-    <script src="js/jquery-3.6.0.min.js" ></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </nav>
-=======
   </nav>
   
 
   <!-- <script src="js/jquery-3.6.0.min.js" ></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script> -->
->>>>>>> bf794804aaf0390b5a4d39711bd79ce505663249
