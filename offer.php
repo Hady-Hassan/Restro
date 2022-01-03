8af093abbceb23b82d739a6128c4218dab7d6980
Mk662000 <mk840166@gmail.com>
2022-01-03 03:26:14 +0200

    offer

---
+++ offer.php
@@ -0,0 +1,28 @@
+<!Doctype html>
+<html>
+        <title> Offers section </title>
+
+    <body class="bd">
+        <?php include("navbar.php"); ?>
+        
+        <div class="offer-details">
+            <div class="offer-details-content">
+                <div class="offer-details-content-image">
+                    <img src="">
+                </div>
+
+<h3>All</h3>
+  
+  <div class ="offer text-center">
+    <img class="img" src="assests/pizza.jpg" style="width: 18rem;">
+    <h5 class="card-title">Pizza</h5>
+    <h5> Description </h5>
+    <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+    <del> <h3>$25.00</h3></del>
+    <h3>$20.00</h3>
+    <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1" id="spinner" style="width: 10%;"/> </p>
+    <input type="submit" name="Add" value="Add to Cart">
+  </div>
+  
+</body>
+</html>
