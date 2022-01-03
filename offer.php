35a097fd816c74e7bbf7fa497794886d18fdd94d
Mk662000 <mk840166@gmail.com>
2022-01-03 04:08:13 +0200

    offer

--- offer.php
+++ offer.php
@@ -1,30 +1,105 @@
 <!Doctype html>
 <html>
-        <title> Offers section </title>
+<title> Offers section </title>
 
-    <body class="bd">
-        <?php include("navbar.php"); ?>
-        
-        <div class="offer-details">
-            <div class="offer-details-content">
-                <div class="offer-details-content-image">
-                    <img src="">
-                </div>
+<body class="bd">
+  <?php include("navbar.php"); ?>
 
-<h3>All</h3>
- <div class="row text-center">
-  <div class ="offer text-center col-md-4 " style="border: 3px solid #FF9900;">
-    <img class="img" src="assests/pizza.jpg" style="width: 18rem;">
-    <h5 class="card-title">Pizza</h5>
-    <h5> Description </h5>
-    <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
-    <del> <h3>$25.00</h3> </del>
-    <h3>$20.00</h3>
-    <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1" id="spinner" style="width: 10%;"/> </p>
-    <input type="submit" name="Add" value="Add to Cart">
-  </div>
-  </div>
-  <?php include("footer.php"); ?>
+  <div class="offer-details">
+    <div class="offer-details-content">
+      <div class="offer-details-content-image">
+        <img src="">
+      </div>
+
+      <h3>All</h3>
+
+      <div class="container">
+        <div class="row justify-content-center text-left">
+
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
+
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
+
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
+
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
 
 
-  
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
+
+          <div class="offer text-center col-md-3 " style="border: 3px solid #FF9900;">
+            <img class="img" src="assets/pizza.jpg" style="width: 18rem;">
+            <h5 class="card-title">Pizza</h5>
+            <h5> Description </h5>
+            <p class="card-text" style="margin: 20px;"> 1 medium pizza, mushroom,</br> mozzarella cheese,tomato</p>
+            <del>
+              <h3>$25.00</h3>
+            </del>
+            <h3>$20.00</h3>
+            <p class="card-text" style="margin: 20px;"> <input type="number" value="1" min="1" max="1000" step="1"
+                id="spinner" style="width: 10%;" /> </p>
+            <input type="submit" name="Add" value="Add to Cart">
+          </div>
+        </div>
+      </div>
+      <?php include("footer.php"); ?>
